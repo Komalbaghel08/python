4e91bcee9fa33f99ce580d3c24588d5e153c253c
Komalbaghel08 <baghelkomal512@gmail.com>
2025-04-22 16:13:54 +0530

    mm

---
+++ DataTypes/numeric.py
@@ -0,0 +1,37 @@
+# Type Of Numeric Datatype
+
+# 1.Int
+# yha pe sirf divide(/) wala float hoga <--- important h interview ke liye --->
+x=10
+y=20
+z1=x+y
+z2=x-y
+z3=x*y
+z4=x/y
+z5=x**y
+z6=x%y
+z7=x//y
+print(type,z4)
+
+
+# 2. float
+# Ab sabka datatype float hoga
+x=10.5
+y=20.4
+z1=x+y
+z2=x-y
+z3=x*y
+z4=x/y
+z5=x**y
+z6=x%y
+z7=x//y
+print(type,z1,z2,z3,z4,z5,z6,z7)
+print(x is y)
+
+
+# 3. complex
+x=10
+y=20
+z=x+y
+print(x)
+print (type(z))
---
+++ DataTypes/ordered.py
@@ -0,0 +1,7 @@
+# Type Of Ordered Datatype
+
+#  1. String
+s="python"
+print(s)
+print(type(s))
+print(id(s))
