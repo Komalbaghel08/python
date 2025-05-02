059c8ef9d1149790135031b0c1771b9e468dc2b0
Komalbaghel08 <baghelkomal512@gmail.com>
2025-05-02 23:58:26 +0530

    nn

--- DataTypes/Dictionary/crudoperation.py
+++ DataTypes/Dictionary/crudoperation.py
@@ -1,6 +1,5 @@
 # CRUD OPERATION
 
-
 # creat
 d ={}
 print(type(d))
@@ -9,16 +8,13 @@ d['key'] = 'value'
 d['hii'] = 'value'
 print(d)
 
-
 # update/edit
 d['key'] = 'hello'
 print(d)   
 
-
 # get/read value
 x=d['key']
 print(x)
-
 
 # delete
 d.pop('key')
