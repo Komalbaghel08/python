dc7a563f7d6d9903053c6d58e56ef13b94c92d40
Komalbaghel08 <baghelkomal512@gmail.com>
2025-05-04 10:35:52 +0530

    update

---
+++ Datatypes__Basic PythonPractice Ques/areaOfCircle.py
@@ -0,0 +1,8 @@
+# Take input from user
+radius = float(input("Enter radius of the circle: "))
+
+# Calculate area
+area = 3.14 * radius * radius
+
+# Print result
+print("Area of the circle is:", area)
---
+++ Datatypes__Basic PythonPractice Ques/swapTwoNum_usinMultiplicationDivision.py
@@ -0,0 +1,15 @@
+a = 4
+b = 2
+
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Swap using multiplication and division
+a = a * b  # a = 8
+b = a / b  # b = 4
+a = a / b  # a = 2
+
+print("After swapping:")
+print("a =", int(a))
+print("b =", int(b))
