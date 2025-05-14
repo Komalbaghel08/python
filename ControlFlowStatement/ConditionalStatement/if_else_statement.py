60d3b83844ba2a8c5321a32015cd77d547b03f7c
Komalbaghel08 <baghelkomal512@gmail.com>
2025-05-14 11:02:47 +0530

    neww

---
+++ ControlFlowStatement/ConditionalStatement/if_else_statement.py
@@ -0,0 +1,5 @@
+age = int(input("enter your age"))
+if age>18:
+    print("you can apply for license")
+else:
+    print("itni jaldi kya hai")
---
+++ ControlFlowStatement/ConditionalStatement/ifstatement.py
@@ -0,0 +1,4 @@
+age = int(input("enter your age"))
+if age>18:
+    print("you can apply for license")
+print("speed thrills but kills")  
