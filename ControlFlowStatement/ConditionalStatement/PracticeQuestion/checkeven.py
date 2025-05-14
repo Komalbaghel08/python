c05af0f9dc8856c6a052451285f8e20b025bf7e8
Komalbaghel08 <baghelkomal512@gmail.com>
2025-05-14 11:47:44 +0530

    update

---
+++ ControlFlowStatement/ConditionalStatement/PracticeQuestion/checkeven.py
@@ -0,0 +1,5 @@
+num = int(input("ente a number"))
+if num%2 == 0:
+    print(f"Number{num}is even")
+else:
+    print(f"Number{num}is odd")    
---
+++ ControlFlowStatement/ConditionalStatement/if_elif_elseStatement.py
@@ -0,0 +1,10 @@
+handsome = 'false'
+good_salary = 'true'
+if handsome == 'true' and good_salary == 'true':
+    print("you will marry with a super model")
+elif handsome!= 'true' and good_salary == 'true':
+    print("you will marry with a beautifull girl")
+elif handsome == 'true' and good_salary != 'true':
+    print("you will marry with a girl")
+else:
+    print("Bhagwan Bharose!!")
