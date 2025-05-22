2ea69219e7cd04cfacd27599058770ac937ac39c
Komalbaghel08 <baghelkomal512@gmail.com>
2025-05-22 08:28:33 +0530

    new

--- Function/Practice_que/palindrome.py
+++ Function/Practice_que/palindrome.py
@@ -1,2 +1,2 @@
-def is_palindrome(s):
+# def is_palindrome(s):
     
--- Function/Practice_que/sum_2_num.py
+++ Function/Practice_que/sum_2_num.py
@@ -1,3 +1,9 @@
-def add(a,b):
-    return a+b
-print(add(2,3))
+# def add(a,b):
+#     return a+b
+# print(add(2,3))
+
+
+# sum of numbers
+def list_sum(numbers):
+    return sum(numbers)
+print(list_sum([10,22,3]))
