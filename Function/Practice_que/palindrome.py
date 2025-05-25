4610f245165ec5d485238fe468e1dde5ecbdd9f3
Komalbaghel08 <baghelkomal512@gmail.com>
2025-05-25 10:30:19 +0530

    new

--- Function/Practice_que/palindrome.py
+++ Function/Practice_que/palindrome.py
@@ -1,2 +1,14 @@
-# def is_palindrome(s):
+n = input("enter a number: ")
+# new = "".join(reversed(n))
+
+# if n == new:
+#     print(f'Given {n} is palindrome')
+# else :
+#     print(f'Given {n}  is not palindrome') 
     
+     
+    #  Second way 
+if n == n[::-1] :
+    print(f'Given {n} is palindrom.')
+else :
+    print(f'Given {n} is not palindrom.')
