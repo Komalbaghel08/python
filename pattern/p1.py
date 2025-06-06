04890832a4fe60a470a134cde7b47b742fe5b95c
Komalbaghel08 <baghelkomal512@gmail.com>
2025-06-06 22:42:19 +0530

    new

--- pattern/p1.py
+++ pattern/p1.py
@@ -1,3 +1,8 @@
+# n = int(input('enter the numbers of rows:'))
+# for i in range (1,n+1):
+#     print(" "*(n-i),"*"*i)
+
+
 n = int(input('enter the numbers of rows:'))
 for i in range (1,n+1):
-    print(" "*(n-i),"*"*i)
+    print(" "*(n-i),"*"*(2*i-1))
