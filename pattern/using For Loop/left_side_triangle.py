a50e793eeef74478e1d002d80214eefc39c5c29c
Komalbaghel08 <baghelkomal512@gmail.com>
2025-06-15 19:17:45 +0530

    new

--- pattern/using For Loop/left_side_triangle.py
+++ pattern/using For Loop/left_side_triangle.py
@@ -25,3 +25,20 @@ for i in range(1, row+1):
     for j in range(1, i + 1):
         print(j, end='')
     print()
+
+
+
+
+# O/P:--
+# 10
+# 10 8
+# 10 8 6
+# 10 8 6 4
+# 10 8 6 4 2
+# code for above output
+k=[]
+for i in range(10,1,-2):
+ k.append(i)
+for j in k:
+ print(j,end=" ")
+print()
