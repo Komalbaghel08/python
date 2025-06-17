8b032d56c0f25df2c7192f395cee7cfef95fd41c
Komalbaghel08 <baghelkomal512@gmail.com>
2025-06-17 15:33:48 +0530

    new

--- pattern/using For Loop/triangle.py
+++ pattern/using For Loop/triangle.py
@@ -13,3 +13,19 @@ for i in range(0,n+1):
 m=n-1
 for i in range(m,0,-1):
  print("* "*i)
+ 
+ # *
+# **
+# ***
+# ****
+# *****
+# *****
+# ****
+# ***
+# **
+# *
+n=int(input("Enter the number of rows: "))
+for i in range(0,n+1):
+ print(" "*(n-i),"*"*i)
+for i in range(n,0,-1):
+ print(" "*(n-i),"*"*i)
