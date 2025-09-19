0fbd84110db826e890fb0cc54866ede2305de723
Komalbaghel08 <baghelkomal512@gmail.com>
2025-09-19 22:54:09 +0530

    update

--- practice.py
+++ practice.py
@@ -1,4 +1,11 @@
-import keyword
-x = keyword.kwlist
-print("total keyword =",len(x))
-print("keyword ",x)
+# import keyword
+# x = keyword.kwlist
+# print("total keyword =",len(x))
+# print("keyword ",x)
+
+
+city = ["Bhopal", "Indore", "Jabalpur"]
+x, y, z = city
+print(x)
+print(y)
+print(z)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+import keyword
+x = keyword.kwlist
+print("total keyword =",len(x))
+print("keyword ",x)
