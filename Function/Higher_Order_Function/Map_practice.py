f5b2eea29410d7c6b8bf1465de18596b743fdfac
Komalbaghel08 <baghelkomal512@gmail.com>
2025-10-06 12:06:09 +0530

    practice

---
+++ Function/Higher_Order_Function/Map_practice.py
@@ -0,0 +1,24 @@
+# square 
+'''l = [2,3,4,5]
+def sq(n):
+    return n*n
+x = map(sq,l)
+print(x)
+print(list(x))'''
+
+# Cube
+'''l = [2,4,6,7,8,9]
+def cube(m):
+    return m*m
+y = map(cube,l)
+print(list(y))'''
+
+
+s = "komal"
+def add(n):
+    x = ord(n)
+    return x
+x = map(add,s)
+print(x)
+print(list(x))
+    
