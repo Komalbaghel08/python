f75ed531b43c39ad5c51ef3f73ffbf9669478acc
Komalbaghel08 <baghelkomal512@gmail.com>
2025-12-10 21:39:38 +0530

    new file

---
+++ practice1.py
@@ -0,0 +1,15 @@
+# Simple Python Program
+
+def greet_user():
+    name = input("Enter your name: ")
+    print(f"Hey {name}, hope you're vibing well today!")
+
+def add_numbers(a, b):
+    return a + b
+
+greet_user()
+
+num1 = 10
+num2 = 20
+
+print("Sum =", add_numbers(num1, num2))
