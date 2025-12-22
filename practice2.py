102d85fa889fc1529734346977a2bc66b34b123b
Komalbaghel08 <baghelkomal512@gmail.com>
2025-12-22 21:25:57 +0530

    factorial

---
+++ practice2.py
@@ -0,0 +1,20 @@
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# a, b = b, a
+
+# print("After swapping:")
+# print("a =", a)
+# print("b =", b)
+
+# factorial
+n = int(input("Enter a number: "))
+
+fact = 1
+i = 1
+
+while i <= n:
+    fact = fact * i
+    i += 1
+
+print("Factorial =", fact)
