185fba4154cebbb96e47133e53cb8542776963d1
Komalbaghel08 <baghelkomal512@gmail.com>
2026-01-22 20:14:49 +0530

    new file

--- practice/prime.py
+++ practice/prime.py
@@ -9,3 +9,14 @@ if count == 2:
     print("Prime number")
 else:
     print("Not a prime number")
+
+# Optimized version
+n = int(input("Enter number: "))
+
+for i in range(2, n):
+    if n % i == 0:
+        print("Not Prime")
+        break
+else:
+    print("Prime")
+
