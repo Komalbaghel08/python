4477cf3d3a87780e915ac73c9c295d892eee1015
Komalbaghel08 <baghelkomal512@gmail.com>
2026-02-01 14:51:31 +0530

    practice

---
+++ PatternPractice/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+r = int(input("Enter number of rows: "))
+# for i in range(1,r+1):
+#     print("* "*i)
--- PatternPractice/triangle.py
+++ PatternPractice/triangle.py
@@ -1,4 +1,29 @@
 # triangle pattern
+# r = int(input("Enter number of rows: "))
+# for i in range(1,r+1):
+#     print("* "*i)
+    
+""" 
+sq. number pattern
+1
+1 2
+1 2 3
+1 2 3 4
+"""
+
+# r = int(input("Enter number of rows: "))
+# for i in range(1,r+1):
+#     for j in range(1,i+1):
+#         print(j, end=" ")
+#     print()
+
+'''
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+'''
 r = int(input("Enter number of rows: "))
 for i in range(1,r+1):
-    print("* "*i)
+    print((str(i)+" ")*i)
