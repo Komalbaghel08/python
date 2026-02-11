6c76c661f50984e905b5d11f6778bf58bcc007a4
Komalbaghel08 <baghelkomal512@gmail.com>
2026-02-11 17:40:09 +0530

    palindrome

--- Revise/demo1.py
+++ Revise/demo1.py
@@ -4,10 +4,25 @@
 #     rev = i + rev
 # print(rev)    
 
-def rev_string(s):
-    rev =''
+'''using function'''
+# def rev_string(s):
+#     rev =''
+#     for i in s:
+#         rev = str(i)+rev
+#     return rev
+# s = input("enter string :")
+# print("reversed string: " , rev_string(s))
+
+
+'''palindrome'''
+def is_palindrome(s):
+    rev = ""
+    s = str(s)
     for i in s:
-        rev = str(i)+rev
-    return rev
+        rev = i+rev
+    if s == rev:
+        return "palindrome"
+    else:
+        return "not palindrome"
 s = input("enter string :")
-print("reversed string: " , rev_string(s))
+print(is_palindrome(s))
