10ed73e53dd2f4f18e3801b8b87292604444bbbb
Komalbaghel08 <baghelkomal512@gmail.com>
2026-02-11 17:33:31 +0530

    practice questions

--- Revise/demo1.py
+++ Revise/demo1.py
@@ -1,5 +1,13 @@
-s = "python"
-rev = ""
-for i in s:
-    rev = i + rev
-print(rev)    
+# s = "python"
+# rev = ""
+# for i in s:
+#     rev = i + rev
+# print(rev)    
+
+def rev_string(s):
+    rev =''
+    for i in s:
+        rev = str(i)+rev
+    return rev
+s = input("enter string :")
+print("reversed string: " , rev_string(s))
