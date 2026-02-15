6542d648155efb21d5c6b4daa4d66970136201b3
Komalbaghel08 <baghelkomal512@gmail.com>
2026-02-15 13:21:19 +0530

    practice

--- Revise/prac.py
+++ Revise/prac.py
@@ -1,17 +1,35 @@
-def count_pairs(arr, k):
-    remainder_count = [0] * k
-    count = 0
+# def count_pairs(arr, k):
+#     remainder_count = [0] * k
+#     count = 0
 
-    for num in arr:
-        remainder = num % k
-        complement = (k - remainder) % k
-        count += remainder_count[complement]
-        remainder_count[remainder] += 1
+#     for num in arr:
+#         remainder = num % k
+#         complement = (k - remainder) % k
+#         count += remainder_count[complement]
+#         remainder_count[remainder] += 1
 
-    return count
+#     return count
 
 
-# Example
-arr = [2, 2, 1, 7, 5, 3]
-k = 4
-print(count_pairs(arr, k))
+# # Example
+# arr = [2, 2, 1, 7, 5, 3]
+# k = 4
+# print(count_pairs(arr, k))
+
+
+
+
+def first_non_repeating(s):
+    freq = {}
+
+    for ch in s:
+        freq[ch] = freq.get(ch, 0) + 1
+
+    for ch in s:
+        if freq[ch] == 1:
+            return ch
+
+    return -1
+
+
+print(first_non_repeating("aabccdeff"))
