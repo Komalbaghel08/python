73d42c7a7f2af9d6a841918533c9ba65e44fdcdd
Komalbaghel08 <baghelkomal512@gmail.com>
2026-03-24 21:34:54 +0530

    new file

--- exercise.py
+++ exercise.py
@@ -1,8 +1,6 @@
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
-
 op = input("Enter operator (+, -, *, /): ")
-
 if op == "+":
     print(a + b)
 elif op == "-":
@@ -13,5 +11,3 @@ elif op == "/":
     print(a / b)
 else:
     print("Invalid operator")
-    
-    
