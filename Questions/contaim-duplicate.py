3c650d9834107059afaa3d9e275eb4b5f4fefc82
Komalbaghel08 <baghelkomal512@gmail.com>
2026-05-09 17:16:01 +0530

    new file

---
+++ Questions/contaim-duplicate.py
@@ -0,0 +1,20 @@
+# Contains Duplicate
+
+def contains_duplicate(nums):
+    seen = set()
+
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+
+    return False
+
+
+# Example
+arr = [1, 2, 3, 4, 2]
+
+if contains_duplicate(arr):
+    print("Duplicate element found")
+else:
+    print("No duplicate element")
