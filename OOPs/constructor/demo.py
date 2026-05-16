bdf21aff15bc151c931b5dbb45d09c335cc8d681
Komalbaghel08 <baghelkomal512@gmail.com>
2026-05-16 22:58:50 +0530

    new file

--- OOPs/constructor/demo.py
+++ OOPs/constructor/demo.py
@@ -7,10 +7,8 @@ class student:
         self .p = phone
 # obj = student    
 # print(obj.n,obj.c,obj.p)   ----error
-
 # obj = student() 
 # print(obj.n,obj.c,obj.p)  ---->error
-
 obj1 = student("komal","bhopal",87695)
 obj2 = student("komall","Rewa",6432345)
 print(obj1.n,obj1.c,obj1.p)
