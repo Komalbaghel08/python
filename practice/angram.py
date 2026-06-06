8cbe1d0723cb16616db8a73c6ac8b3558a9f08d9
Komalbaghel08 <baghelkomal512@gmail.com>
2026-06-06 22:01:20 +0530

    new file

---
+++ practice/angram.py
@@ -0,0 +1,7 @@
+# angram checker
+def angram(s1, s2):
+    s1 = s1.replace(" ", "").lower()
+    s2 = s2.replace(" ", "").lower()
+    return sorted(s1) == sorted(s2)
+print(angram("listen", "silent"))  # True
+print(angram("hello", "world"))  # False
