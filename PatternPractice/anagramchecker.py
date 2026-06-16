49864fc56d137c48dd919ca2ead1bcf60a963731
Komalbaghel08 <baghelkomal512@gmail.com>
2026-06-16 20:00:59 +0530

    new file

---
+++ PatternPractice/anagramchecker.py
@@ -0,0 +1,9 @@
+# anagram checker
+def are_anagrams(s1, s2):   
+    
+    # Remove spaces and convert to lowercase
+    s1 = s1.replace(" ", "").lower()
+    s2 = s2.replace(" ", "").lower()
+    
+    # Sort the characters of both strings and compare
+    return sorted(s1) == sorted(s2)
